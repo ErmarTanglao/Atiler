a6121451c4f629d28615e7e5342d5d2faf0fcb27
ErmarTanglao <43150372+ErmarTanglao@users.noreply.github.com>
2018-09-13 11:39:17 -0400

    Sketch 2 particle system with dom working

--- sketch2/sketch2.js
+++ sketch2/sketch2.js
@@ -3,10 +3,15 @@ var mouseclicks = [];
 function setup(){
 	createCanvas(720,480);
 	var button = createButton("add particle");
+	button.mousePressed(addparticle);
+	noStroke();
 }
 
 function draw(){
+	//translate(width/2,height,2);
+	background(255);
 	for(var i=0; i<mouseclicks.length; i++){
+		fill(mouseclicks[i].color);
 		ellipse(mouseclicks[i].x,mouseclicks[i].y,10);
 		mouseclicks[i].x = mouseclicks[i].x + mouseclicks[i].xspeed;
 		mouseclicks[i].y = mouseclicks[i].y + mouseclicks[i].yspeed;
@@ -15,10 +20,15 @@ function draw(){
 
 function addparticle(){
 	var clickposition = {
-		x: mouseX,
-		y: mouseY,
+		x: random(width),
+		y: random(height),
 		xspeed: random(-2,2),
-		yspeed: random(-2,2)
+		yspeed: random(-2,2),
+		color: color(random(255), random(255), random(255))
 	}
 	mouseclicks.push(clickposition);
+}
+
+function mouseClicked(){
+	createP("Hello!");
 }
