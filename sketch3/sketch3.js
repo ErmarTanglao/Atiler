79901149ea87b30fb991cbe3021dfb957f56ca8c
ErmarTanglao <43150372+ErmarTanglao@users.noreply.github.com>
2018-09-13 12:11:09 -0400

    Put bubble back to middle when leaves boundary

--- sketch3/sketch3.js
+++ sketch3/sketch3.js
@@ -1,13 +1,28 @@
 var mysound;
+var bubble;
 
 function setup(){
 	createCanvas(720,480);
 	mysound = createAudio("TT.mp3"); //to make it work upload to webspace
-
+	bubble = {
+		x: width/2,
+		y: height/2,
+		r: 15
+	}
 }
 
 function draw(){
-	ellipse(width/2,height/2,30)
+	ellipse(bubble.x,bubble.y,bubble.r * 2);
+	bubble.x = bubble.x + random(-5,5);
+	bubble.y = bubble.y + random(-5,5);
+	if(bubble.x > width || bubble.x < 0){
+		bubble.x = width/2;
+		bubble.y = height/2;
+	};
+	if(bubble.y > height || bubble.y < 0){
+		bubble.y = height/2;
+		bubble.x = width/2;
+	};
 }
 
 function mouseClicked(){
@@ -15,8 +30,8 @@ function mouseClicked(){
 	//	if(mouseY < height/2 +15 && mouseY > height/2 - 15)
 	//	fill(100)
 	//}
-	var clickeddistance = dist(width/2, height/2, mouseX, mouseY);
-	if(clickeddistance < 15){
+	var clickeddistance = dist(bubble.x, bubble.y, mouseX, mouseY);
+	if(clickeddistance < bubble.r){
 		fill(100)
 		mysound.play();
 	}
