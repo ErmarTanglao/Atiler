f30790d79074e051aa1191cb32757016faa655ce
ErmarTanglao <43150372+ErmarTanglao@users.noreply.github.com>
2018-09-13 10:47:37 -0400

    mouseclicks works

---
+++ sketch2/sketch2.js
@@ -0,0 +1,19 @@
+var mouseclicks = [];
+
+function setup(){
+	createCanvas(720,480);
+}
+
+function draw(){
+	for(var i=0; i<mouseclicks.length; i++){
+		ellipse(mouseclicks[i].x,mouseclicks[i].y,10)
+	}
+}
+
+function mouseClicked(){
+	var clickposition = {
+		x: mouseX,
+		y: mouseY
+	}
+	mouseclicks.push(clickposition);
+}
